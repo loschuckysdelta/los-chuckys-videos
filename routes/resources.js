f76a9cf6cb4dc80017f691f1d9319427eed911bf
BOT API <los.chuckys.delta@gmail.com>
2026-09-21 00:07:53 -0300

    Delete routes directory

--- routes/resources.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express');
-const collection = require('../controllers/collection');
-
-const router = Router();
-
-router.get('/', collection.getResources);
-
-module.exports = router;
